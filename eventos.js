8757241ad7835c8d0dd0435e3a1685fb66dd56ee
Santi <moreirasantiagoc@gmail.com>
2023-08-09 17:52:12 -0300

    solution alert

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
 const divbtn = document.getElementById("btncontainer");
+const btn = document.getElementById('btnsaludar');
+
+btn.addEventListener('click', function btn() {
+    alert('Hola!');
+    event.stopPropagation();
+});
 
 divbtn.addEventListener('click', function alertd() {
     alert('Hola! Soy el div');
